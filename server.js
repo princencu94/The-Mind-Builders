4c2733bf93ad3c60e8c1aed95d31a41f80506c01
princencu94 <56699089+princencu94@users.noreply.github.com>
2021-11-23 15:11:58 +0200

    Added new configurations to server

--- server.js
+++ server.js
@@ -24,4 +24,44 @@ let transporter = nodemailer.createTransport({
       type: "OAuth2",
       user: process.env.EMAIL,
       pass: process.env.WORD,
-      clientId: process.
+      clientId: process.env.OAUTH_CLIENTID,
+      clientSecret: process.env.OAUTH_CLIENT_SECRET,
+      refreshToken: process.env.OAUTH_REFRESH_TOKEN,
+      expires: 1484314697598
+    },
+    tls: {
+        rejectUnauthorized: false
+    }
+   });
+
+   transporter.verify((err, success) => {
+    err
+      ? console.log(err)
+      : console.log(`=== Server is ready to take messages: ${success} ===`);
+    });
+
+    app.post("/send", function (req, res) {
+        let mailOptions = {
+          from: "princencu@yahoo.com",
+          to: `${req.body.assignJobDetails.email}`,
+          subject: "Job Assignment",
+          text: "Hi You are getting this email as you have been assigned for this job please follow this link http://localhost:3000/sign-up",
+        };
+       
+    transporter.sendMail(mailOptions, function (err, data) {
+        if (err) {
+            res.json({
+            status: "fail",
+            });
+        } else {
+            console.log("== Message Sent ==");
+            res.json({
+            status: "success",
+            });
+        }
+            });
+    });
+app.listen(port, error => {
+    if(error) throw console.error;
+    console.log('Server running on Port ' + port);
+})
