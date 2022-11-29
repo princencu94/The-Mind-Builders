ba723fb108d7a6583b0b9047b1071c363f64c3e8
Prince Ncube <princencu94@gmail.com>
2022-11-29 20:41:09 +0200

    Fixed File input

--- src/components/request-new-talent/request-new-talent.component.jsx
+++ src/components/request-new-talent/request-new-talent.component.jsx
@@ -25,9 +25,6 @@ const validate = values => {
 
 
 
-  if (!values.file) {
-    errors.file = 'Required*';
-  } 
 
   if (!values.lastname) {
     errors.lastname = 'Required*';
@@ -57,7 +54,6 @@ const RequestNewTalentForm = () => {
           jobtitle:'',
           phone:'',
           seeking:'',
-          file:'',
         },
         validate,
         onSubmit: values => {
@@ -318,60 +314,6 @@ const RequestNewTalentForm = () => {
             </div>
           </div>
 
-          <div className="pt-8">
-            
-
-            <div className="mt-6 grid grid-cols-1 gap-y-6 gap-x-4 sm:grid-cols-6">
-              
-              <div className="sm:col-span-6">
-                <label htmlFor="cover-photo" className="block text-sm font-medium text-gray-700">
-                  Resume
-                </label>
-                <div className="mt-1 flex justify-center px-6 pt-5 pb-6 border-2 border-gray-300 border-dashed rounded-md">
-                  <div className="space-y-1 text-center">
-                    <svg
-                      className="mx-auto h-12 w-12 text-gray-400"
-                      stroke="currentColor"
-                      fill="none"
-                      viewBox="0 0 48 48"
-                      aria-hidden="true"
-                    >
-                      <path
-                        d="M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02"
-                        strokeWidth={2}
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                      />
-                    </svg>
-                    <div className="flex text-sm text-gray-600">
-                      <label
-                        htmlFor="file-upload"
-                        className="relative cursor-pointer bg-white rounded-md font-medium text-black hover:text-yellow-300 focus-within:outline-none focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-yellow-300"
-                      >
-                        
-                        <input 
-                        id="file-upload" 
-                        name="file" 
-                        type="file" 
-                        
-                        onChange={formik.handleChange}
-                        onBlur={formik.handleBlur}
-                        value={formik.values.file}
-                        />
-                      </label>
-                      <p className="pl-1"></p>
-                    </div>
-                    <p className="text-xs text-gray-500">PDF/Docx/Doc up to 10MB</p>
-
-                  </div>
-                </div>
-                {formik.touched.file && formik.errors.file ? (
-                <div><p className="text-red-600 text-sm">{formik.errors.file}</p></div>
-                ) : null}
-              </div>
-         
-            </div>
-          </div>
         </div>
   
         <div className="pt-5">
